a19e0034785f10d611c94e802af3968f7f5a7b1d
StepanHokobyan <31405336+StepanHokobyan@users.noreply.github.com>
2019-03-29 17:43:42 +0400

    Update Program.cs

--- ComplexNumbers/Program.cs
+++ ComplexNumbers/Program.cs
@@ -6,15 +6,20 @@ namespace ComplexNumbers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the real Number");
+            Console.WriteLine("Enter the first real Number");
             int a = int.Parse(Console.ReadLine());
-            Console.WriteLine("Enter The fake number");
+            Console.WriteLine("Enter The first fake number");
             int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the second real Number");
+            int d = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter The second fake number");
+            int e = int.Parse(Console.ReadLine());
+             
             Console.WriteLine("Enter the I");
             Console.WriteLine("for example i or i^2 etc");
             string c = Console.ReadLine();
             ComplexNum complexNum1 = new ComplexNum(a, b, c);
-            ComplexNum complexNum2 = new ComplexNum(13, 6, "i^2");
+            ComplexNum complexNum2 = new ComplexNum(d, e, c);
             Console.WriteLine(complexNum1.Sum(complexNum1, complexNum2));
             Console.WriteLine(complexNum1.Subtraction(complexNum1, complexNum2));
             Console.WriteLine(complexNum1.Multiplication(complexNum1, complexNum2));
